f16ea6b995e2e61597c377911501fbda15d97bd4
malala <mawilimay@gmail.com>
2024-05-05 15:45:54 +0200

    Added GetAllPosts

---
+++ src/components/GetAllPosts.jsx
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const GetAllPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/tweets');
+        setPosts(response.data);
+      } catch (error) {
+        setError('Failed to fetch posts');
+        console.error('Fetching error:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>All Posts</h2>
+      {error && <p>{error}</p>}
+      <ul>
+        {posts.map(post => (
+          <li key={post._id}>
+            <p>@{post.username}</p>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GetAllPosts;

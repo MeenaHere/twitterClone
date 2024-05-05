0a1b4c2cae2340fe75b9971c82dfd1e8ebf062ed
ImMeena <meenasingh754@gmail.com>
2024-05-05 20:29:18 +0200

    updated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Profile from "./components/profile/Profile";
 import Followers from "./components/profile/Followers";
 import Following from "./components/profile/Following";
 
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/users/:id" element={<Profile />} />
+        <Route path="/" element={<Login />} />
         <Route path="/users/:id/followers/" element={<Followers />} />
         <Route path="/users/:id/following/" element={<Following />} />
       </Routes>

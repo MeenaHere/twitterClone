14af679f588bb08d9ec73e4fcb139f92014693eb
ImMeena <meenasingh754@gmail.com>
2024-05-07 17:51:49 +0200

    changed userId in spec.cy

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("ProfileInfo component", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173/users/662ac6643107cfece4945ed7");
+    cy.visit("http://localhost:5173/users/66355470dd36b721249da823");
   });
 
   it("should contain Trends for you", () => {

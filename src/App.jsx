f423fbfedb499e6affa8d1ce17d4ebfeaf525305
malala <mawilimay@gmail.com>
2024-05-07 12:30:01 +0200

    changing path / to /home

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );

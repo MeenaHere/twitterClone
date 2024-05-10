256e284915248f9aaea4cc3376f6cea1015dd46e
ImMeena <meenasingh754@gmail.com>
2024-05-10 12:32:01 +0200

    changed back followers,following

--- src/components/profile/Following.jsx
+++ src/components/profile/Following.jsx
@@ -1,32 +1,12 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Col, Image, Row } from "react-bootstrap";
-import { getAllFollowing, getAllUsers, getOneUser } from "../../userServices";
-import Sidebar from "../Sidebar/Sidebar";
-import SearchField from "../RightSide/SearchField";
+import { Link, useParams } from "react-router-dom";
+import { Image } from "react-bootstrap";
+import { getAllFollowing, getAllUsers } from "../../userServices";
 
 function Following() {
   const [following, setfollowing] = useState([]);
   const [users, setUsers] = useState([]);
   const { id } = useParams();
-  const [user, setUser] = useState(null);
-  const [screenSize, setScreenSize] = useState(false);
-
-  const navigate = useNavigate();
-
-  // Fetch user's details from the database using the id
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dbuser = await getOneUser(id);
-        setUser(dbuser);
-        console.log("folloer user", dbuser);
-      } catch (error) {
-        console.error("Error fetching user", error);
-      }
-    };
-    fetchData();
-  }, [id]);
 
   // Fetch all users from db
   useEffect(() => {
@@ -54,96 +34,41 @@ function Following() {
     fetchData();
   }, [id]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      // Check if screen width falls within tablet size range (768px) to hide the trend component on samll screen
-      setScreenSize(window.innerWidth <= 768);
-    };
-
-    // Add event listener for window resize
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const handleGoBack = () => {
-    navigate(-1);
-  };
-
-  if (user) {
-    return (
-      <div className="container">
-        <div className="row">
-          <Col md={4} lg={3}>
-            {!screenSize && <Sidebar />}
-          </Col>
-          <Col xs={10} md={6} lg={6}>
-            <Row>
-              <Col xs={1} md={1} className="display-4">
+  return (
+    <div className="container-fluid" style={{ textTransform: "capitalize" }}>
+      <ul>
+        {following.map((following) => {
+          const matchedUser = users.find(
+            (user) => user._id === following.followingId
+          );
+          if (matchedUser) {
+            return (
+              <li key={following.followingId} className="list-unstyled">
+                <Image
+                  src="\cover1.jpeg"
+                  alt="profile photo"
+                  style={{
+                    width: "4rem",
+                    height: "4rem",
+                  }}
+                  roundedCircle
+                  className="border border-white border-4"
+                />
                 <Link
-                  className=" text-decoration-none text-dark"
-                  onClick={handleGoBack}
+                  to={`/users/${matchedUser._id}`}
+                  className="m-4 text-decoration-none"
                 >
-                  ←
+                  {matchedUser.username}
                 </Link>
-              </Col>
-              <Col xs={5} md={4} className="m-2">
-                <h4 className="fw-bold">{user.fullName}</h4>
-                <p>@{user.username}</p>
-              </Col>
-            </Row>
-            <ul>
-              {following.map((following) => {
-                // Find the user corresponding to the followingId
-                const matchedUser = users.find(
-                  (user) => user._id === following.followingId
-                );
-                if (matchedUser) {
-                  return (
-                    <li key={following.followingId} className="list-unstyled">
-                      <Row>
-                        <Col xs={2} md={2}>
-                          <Image
-                            src="\cover1.jpeg"
-                            alt="profile photo"
-                            style={{
-                              width: "4rem",
-                              height: "4rem",
-                            }}
-                            roundedCircle
-                            className="border border-white border-4"
-                          />
-                        </Col>
-                        <Col xs={3} md={5}>
-                          <Link
-                            to={`/users/${matchedUser._id}`}
-                            className="text-decoration-none"
-                          >
-                            {matchedUser.fullName}
-                            <br />@{matchedUser.username}
-                          </Link>
-                        </Col>
-                      </Row>
-                    </li>
-                  );
-                } else {
-                  return null; // Handle case when followingId doesn't match any user
-                }
-              })}
-            </ul>
-          </Col>
-          <Col md={3} lg={3}>
-            {!screenSize && <SearchField />}
-          </Col>
-        </div>
-      </div>
-    );
-  } else {
-    <div>Loading</div>;
-  }
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Following;

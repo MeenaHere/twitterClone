2f9de2c71b7190e3ab5d670d86ad7dbeaf8e1098
Ranunculus32 <christine.paner@outlook.com>
2024-05-17 15:08:58 +0200

    Update Feed.jsx

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 /* import TweetBox from "./TweetBox"; */
-import "./Feed.css";
+import "../Feed/Feed.css";
 import axios from "axios";
 import { FiSettings } from "react-icons/fi";
-import Post from "./Post";
-import CreatePost from "./CreatePost";
-import Tweetbox from "../Tweetbox";
+import Post from "../Feed/Post";
+import Tweetbox from "../Tweet/Tweetbox";
 /* import Tweetbox from "../Tweetbox"; */
 
 function Feed() {

4d10f5c30892851f1e655a0e4eafb704b2b0ae21
ImMeena <meenasingh754@gmail.com>
2024-05-20 12:52:17 +0200

    fixed post tweet

--- src/components/Tweet/Tweetbox.jsx
+++ src/components/Tweet/Tweetbox.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import axios from 'axios';
 // eslint-disable-next-line no-unused-vars
 import { useNavigate } from 'react-router-dom';
@@ -13,6 +13,7 @@ import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 import './Tweetbox.css';  
+import { getOneUser } from "../../userServices";
 
 
 const BlueFolderOpenIcon = styled(SourceOutlinedIcon)({
@@ -44,16 +45,28 @@ axios.defaults.withCredentials = true;
 
 const Tweetbox = () => {
   const [content, setContent] = useState('');
+  const [user, setUser] = useState(null)
   const navigate = useNavigate();
+
+  const userId = localStorage.getItem('userId');
+    
+    //get a user data from db by using loggedInUserId
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dbuser = await getOneUser(userId);
+        setUser(dbuser);
+        console.log("user", dbuser);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, [userId]);
 
   // Function to create a tweet
   const createTweet = async () => {
-    const userId = localStorage.getItem('userId');
-    const username = localStorage.getItem('username');
-
-   
-
-    if (!userId || !username) {
+    if (!userId) {
       console.error('User is not logged in');
       return;
     }
@@ -61,7 +74,7 @@ const Tweetbox = () => {
     try {
       const response = await axios.post('tweets/create', {
         userId: userId,
-        username: username,
+        username: user.username,
         content: content,
       }, {
         headers: {
